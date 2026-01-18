89d598c7da66896c87be7391e9322c5708626af6
Ozzie <ozzielask@gmail.com>
2026-01-18 16:14:52 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,4 +8,8 @@ function draw() {
   noStroke();
   fill(255, 0, 0,);
   rect(150, 150, 100, 100);
+
+  fill(0, 0, 255);
+  circle(mouseX, mouseY, width*0.01); <!-- Draws a circle that follows the mouse -->
+  
 }
